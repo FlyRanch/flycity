b98ee99a69d687ee6597d55ddb56a08886157983
Theodore Lindsay <thlindsay1@gmail.com>
2014-04-08 15:03:36 -0700

    Updated the Fly object to load data from the matlab WBKin.mat files. Also added some error checking code for the axon-photron synchronization method. Next step is to write a function to interpret the trial type information from the ypos signal.

--- muscle_analysis/flylib.py
+++ muscle_analysis/flylib.py
@@ -7,6 +7,7 @@ import scipy
 import numpy as np
 import quantities as pq
 import scipy.io
+import warnings
 
 param_file = open('params.json','rb')
 params = json.load(param_file)
@@ -14,7 +15,10 @@ param_file.close()
 #rootpath = params['platform_paths'][sys.platform] + params['root_dir']
 
 class Fly(object):
-    """Controler object for fly data"""
+    """Controler object for fly data, FLy is initialized with a 'fly_record dictionary
+    and the object is used to facilitate adding and removing data from this dictionary
+    but does does not itself own the dictionary - if Fly is deleted, the dictionary
+    can still exist"""
     def __init__(self,fly_record):
         self.flynum = fly_record['flynum']
         self.fly_record = fly_record
@@ -24,18 +28,53 @@ class Fly(object):
         self.rootpath = self.params['platform_paths'][sys.platform] + self.params['root_dir']
         self.fly_path = self.rootpath + ('Fly%04d/')%(fly_record['flynum'])
         
+    def load_kine_sequences(self,experiment_name):
+    	"""load all the matlab generated kine sequences for an exp"""
+    	snums = self.fly_record['experiments'][experiment_name]['photron_seq_nums']
+        frmtstr = self.fly_record['experiments'][experiment_name]['solution_format_string']
+        frmtstr += self.fly_record['experiments'][experiment_name]['kine_filename']
+        kinefiles = [self.fly_path + frmtstr%(snum) for snum in snums]
+        kines = [self.load_wbkine(kine_filename) for kine_filename in kinefiles]
+        #add the file string since if the list of sequences for a fly is re-arranged
+        #this key will allow them to be re-sorted in order
+        [k.update({'seq_num':snum}) for k,snum in zip(kines,snums)]
+        self.fly_record['experiments'][experiment_name]['kine_sequences'] = kines
+        
+    def load_wbkine(self,kine_filename):
+    	"""load the matlab generated wb kine and append with some info for convenience"""
+    	kine_data = scipy.io.loadmat(kine_filename)
+    	##now to make life easier add the frame numbers to the dictionary
+    	first_track = np.argwhere(~np.isnan(kine_data['eta_L']))[0][0]+1
+    	last_track = np.shape(kine_data['eta_R'])[0]
+    	#sanity check
+    	def check_first_frame():
+    		kfn = kine_filename.split('/')[-1]
+    		flytracks_path = kine_filename.replace(kfn,'flytracks/')
+        	#get the list of frames
+        	tracklist = filter(lambda x: x.startswith('fly'), os.listdir(flytracks_path))
+        	framenums = [np.int(s.strip('fly.mat')) for s in tracklist]
+        	return np.min(framenums),np.max(framenums)
+        assert first_track == check_first_frame()[0],(first_track,check_first_frame()[0])
+        assert last_track == check_first_frame()[1],(last_track,check_first_frame()[1])
+        kine_data['first_track'] = first_track
+        kine_data['last_track'] = last_track
+        kine_data['frame_nums'] = np.arange(1,last_track)
+        return kine_data
+    	
     def load_photron_sequences(self,experiment_name):
-        """function loads the photron sequences from a given experiment"""
+        """function loads the photron sequences from a given experiment without using
+        matlab file"""
         snums = self.fly_record['experiments'][experiment_name]['photron_seq_nums']
         frmtstr = self.fly_record['experiments'][experiment_name]['solution_format_string']
         seqpaths = [self.fly_path + frmtstr%(snum) for snum in snums]
         seqs = [self.load_solution_seq(seqpath) for seqpath in seqpaths]
         self.fly_record['experiments'][experiment_name]['photron_sequences'] = seqs
-        
+    	
     def load_solution_seq(self,sequence_path):
-        """convenience function to load the sequence from fly tracks function will concatinate
-        the frame numbers into the first row the kine matrxi, also the untracked before the
-        start of the sequence are padded with NaNs"""
+        """convenience function to load the sequence from fly tracks function will cat
+        the frame numbers into the first row the kine matrx, also the untracked frames
+        before the start of the sequence are padded with NaNs - tries to emulate the same 
+        format as the WB kine file"""
         flytracks_path = sequence_path + 'flytracks/'
         #get the list of frames
         tracklist = filter(lambda x: x.startswith('fly'), os.listdir(flytracks_path))
@@ -62,33 +101,48 @@ class Fly(object):
         axon_file = self.fly_path + self.fly_record['experiments'][experiment_name]['axon_file_names'][filenum]
         self.fly_record['experiments'][experiment_name]['axon_data'] = get_axon_signals(axon_file) 
     
-    def get_cam_epochs(self,experiment_name,numframes = 1365,fps = pq.Quantity(6000.0,'Hz')):
+    def get_cam_epochs(self,experiment_name):
         """load and sync the axon and photron data for the capture epochs of a
-        given experiment. Loads the axon data if it is not already loaded"""
-        if 'axon_data' in self.fly_record['experiments'][experiment_name].keys():
-            axondata = self.fly_record['experiments'][experiment_name]['axon_data']
-        else:
-            self.load_axon_data(experiment_name)
-            axondata = self.fly_record['experiments'][experiment_name]['axon_data']
+        given experiment. Loads the axon and photron data if they are not already loaded"""
+        exp = self.fly_record['experiments']['lr_blob_expansion']
+        fps = pq.Quantity(np.float64(exp['photron_frame_rate_Hz']),'Hz')
+        if 'axon_data' not in exp.keys(): self.load_axon_data(experiment_name)
+    	if 'kine_sequences' not in exp.keys(): self.load_kine_sequences(experiment_name)
+        kine_data = exp['kine_sequences']
+        numframes = exp['kine_sequences'][0]['last_track']
         capture_epoch = numframes/fps
-        ax_dt = axondata['CamTrig'].sampling_period
-        ax_dt.units = 's'
+        ax_dt = pq.Quantity(exp['axon_data']['sampling_period'],'s')
         capture_samples = np.ceil(capture_epoch/ax_dt)
-        trig_idx = idx_by_thresh(axondata['CamTrig'])
+        trig_idx = idx_by_thresh(exp['axon_data']['CamTrig'])
         start_idxs = [x[0] for x in trig_idx]
         cam_epochs = [np.arange(np.int(x),np.int(x)+capture_samples,dtype = np.int) for x in start_idxs]
-        frame_idxs = [self.get_frame_idxs(epoch,axondata) for epoch in cam_epochs]
-        times = axondata['CamTrig'].times
-        self.fly_record['experiments'][experiment_name]['cam_epochs'] = cam_epochs
-        self.fly_record['experiments'][experiment_name]['frame_idxs'] = frame_idxs
-        self.fly_record['experiments'][experiment_name]['frame_times'] = [times[idx] for idx in frame_idxs]
+        def fallback_frame_idx(cam_epoch):
+        	idx = np.array(np.ceil(np.linspace(cam_epoch[0],cam_epoch[-1],numframes)),dtype = int)
+        	return idx
+        frame_idx_list = list()
+        for i,epoch in enumerate(cam_epochs):
+        	try:
+        		frame_idxs = self.get_frame_idxs(epoch,exp['axon_data'])
+        	except IndexError:
+        		warnings.warn("problem extracting idxs from camera_sync_signal for epoch num %s using linearly spaced index idx's over the camera epoch instead"%(i))
+        		frame_idxs = fallback_frame_idx(epoch)
+        	if not(np.shape(frame_idxs)[0] == numframes):
+        		import warnings
+        		warnings.warn("problem extracting idxs from camera_sync_signal for epoch num %s using linearly spaced index idx's over the camera epoch instead"%(i))        		
+        		frame_idxs = fallback_frame_idx(epoch)
+        	frame_idx_list.append(frame_idxs)
+        times = exp['axon_data']['times']
+        [d.update({'axon_epoch':epoch}) for d,epoch in zip(exp['kine_sequences'],cam_epochs)]
+        [d.update({'axon_idxs':idxs}) for d,idxs in zip(exp['kine_sequences'],frame_idx_list)]
+        [d.update({'axon_times':times[idxs]}) for d,idxs in zip(exp['kine_sequences'],frame_idx_list)]
+        #exp['frame_idxs'] = frame_idxs
+        #exp['frame_times'] = [times[idx] for idx in frame_idxs]
     
     def get_frame_idxs(self,cam_epoch,axondata):
         """exctract the sync pulse from the camera epochs"""
         frame_idxs = [x[0]+cam_epoch[0] for x in idx_by_thresh(axondata['CamSync'][cam_epoch]*-1,-3.5)]
         frame_idxs[0] -=1
         return frame_idxs
-
 
 def get_axon_signals(filename):
     from neo.io.axonio import AxonIO
@@ -97,7 +151,16 @@ def get_axon_signals(filename):
     header = reader.read_header()
     channel_names = [info['ADCChNames'] for info in header['listADCInfo']]
     channel_units = [info['ADCChUnits'] for info in header['listADCInfo']]
-    signals = dict(zip(channel_names,blocks[0].segments[0].analogsignals))
+    times = blocks[0].segments[0].analogsignals[0].times
+    sampling_period = blocks[0].segments[0].analogsignals[0].sampling_period
+    times.units = 's'
+    sampling_period.units = 's'
+    times = np.array(times)
+    sampling_period = np.float64(sampling_period)
+    signals = [np.array(x) for x in blocks[0].segments[0].analogsignals]
+    signals = dict(zip(channel_names,signals))
+    signals['times'] = times
+    signals['sampling_period'] = sampling_period
     return signals
 
 def idx_by_thresh(signal,thresh = 0.1):
