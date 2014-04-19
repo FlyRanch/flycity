734662b3a9f07517ca50df9680c594c531ec129b
psilentp <thlindsay1@gmail.com>
2014-04-18 17:37:00 -0700

    moving more things around

--- muscle_analysis/neo/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-'''
-Neo is a package for representing electrophysiology data in Python,
-together with support for reading a wide range of neurophysiology file formats
-'''
-
-from neo.core import *
-from neo.io import *
-from neo.version import version as __version__
--- muscle_analysis/neo/test/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Contains tests for neo.core
-
-The subdirectory iotest contains tests for neo.io
-"""
