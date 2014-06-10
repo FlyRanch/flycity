bed9dee2330e9f2e3f0766ae64ea550b09837c54
psilentp <thlindsay1@gmail.com>
2014-06-09 18:16:46 -0700

    made roi measurments

--- flytracker_tethered/muscle_analysis/roi_extraction.py
+++ flytracker_tethered/muscle_analysis/roi_extraction.py
@@ -4,12 +4,12 @@ import h5py
 import flylib
 import db_access as dba
 import numpy as np
-
+i = 17
 imaging_group = [151,153,154,155,156,157,158,159,160,161,162,163,164,165,166,167,168,169]
 fly_db = dba.get_db()
 swarm = flylib.Squadron(fly_db,imaging_group)
-expmnt = swarm.flies[8].experiments[u'img_starfield_t2_rep1']
-
+expmnt = swarm.flies[i].experiments[u'img_starfield_t2_rep1']
+print(imaging_group[i])
 import imaging_viewer
 viewer = imaging_viewer.ImagingViewer(expmnt)
 viewer.run()
