28178b4d6e505700ae44ffe364bc8de0303c7dd9
Florian Muijres <florianmuijres@gmail.com>
2014-10-14 15:07:45 +0200

    removed abundant WORK folder

--- WORK/postproc/D2_BodyKin_presaccade/log_hist.m
+++
@@ -1,9 +0,0 @@
-function log_hist(x,n)
-hist(x,n)
-
-ph = get(gca,'children');
-vn = get(ph,'Vertices');
-vn(:,2) = vn(:,2) + 1;
-set(ph,'Vertices',vn);
-
-set(gca,'yscale','log')
--- WORK/postproc/D_BodyKin/plot_heatmaps_log.m
+++
@@ -1,5 +0,0 @@
-%% heatmap hist log
-
-yx_hist = hist3([y_hist,x_hist], {biny binx});
-yx_hist_log = log(yx_hist);
-imagesc(binx,-biny,yx_hist_log)
